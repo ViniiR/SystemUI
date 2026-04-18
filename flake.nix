1bc88c39564f11c16f16a713797b9e4bee7fe31e
ViniiR <cvrm2004@gmail.com>
2026-04-17 23:02:59 -0300

    init: first window

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+    inputs.nixpkgs.url = "github:nixos/nixpkgs/afbbf774e2087c3d734266c22f96fca2e78d3620";
+    outputs = {
+        self,
+        nixpkgs,
+    }: let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {inherit system;};
+        lib = pkgs.lib;
+    in {
+        devShells.${system}.default = pkgs.mkShell rec {
+            NIX_ENFORCE_PURITY = 0;
+            LD_LIBRARY_PATH = lib.makeLibraryPath packages;
+
+            # Packages available in the User's shell
+            packages = with pkgs; [
+                pkg-config
+                cmake
+                gcc
+                gtk4
+                zsh
+            ];
+
+            shellHook = ''
+                export SHELL=${pkgs.zsh}/bin/zsh
+                exec zsh;
+            '';
+        };
+    };
+}
---
+++ main.c
@@ -0,0 +1,25 @@
+#include <gio/gio.h>
+#include <glib-object.h>
+#include <gtk-4.0/gtk/gtk.h>
+#include <gtk/gtkshortcut.h>
+
+static void activate(GApplication *app, gpointer *user_data) {
+    GtkBuilder *builder = gtk_builder_new_from_file("../src/builder.ui");
+
+    GtkWidget *win = GTK_WIDGET(gtk_builder_get_object(builder, "main-window"));
+
+    gtk_window_set_application(GTK_WINDOW(win), GTK_APPLICATION(app));
+    gtk_window_present(GTK_WINDOW(win));
+}
+
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status = 0;
+
+    app = gtk_application_new("com.vinii.system_ui", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
+}
