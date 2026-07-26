28b6603b2a7c6b78586c522a589d84363950c041
ViniiR <cvrm2004@gmail.com>
2026-07-26 14:42:17 -0300

    feat: migrate dbus call to future

--- src/brightness.rs
+++ src/brightness.rs
@@ -17,26 +17,23 @@ pub fn handle_brightness(builder: &Builder, conn: DBusConnection) -> Result<(), 
         return Err(HandlerError::ObjectError);
     };
 
-    // IIFE
-    (glib::clone!(
-        #[strong]
-        conn,
+    let res = conn.call_future(
+        dbus::BUS_NAME,
+        dbus::Controllers::BRIGHTNESS,
+        &dbus::Controllers::to_interface(dbus::Controllers::BRIGHTNESS),
+        dbus::Methods::GET_BRIGHTNESS,
+        None,
+        Some(VariantTy::TUPLE),
+        DBusCallFlags::NONE,
+        dbus::Timeout::NONE,
+    );
+    glib::MainContext::default().spawn_local(glib::clone!(
         #[strong]
         label,
         #[strong]
         scale,
-        move || {
-            let res = conn.call_sync(
-                dbus::BUS_NAME,
-                dbus::Controllers::BRIGHTNESS,
-                &dbus::Controllers::to_interface(dbus::Controllers::BRIGHTNESS),
-                dbus::Methods::GET_BRIGHTNESS,
-                None,
-                Some(VariantTy::TUPLE),
-                DBusCallFlags::NONE,
-                dbus::Timeout::NONE,
-                gio::Cancellable::NONE,
-            );
+        async move {
+            let res = res.await;
 
             if let Err(e) = &res {
                 g_warning!(None, "DBus call error: {e:?}");
@@ -49,7 +46,7 @@ pub fn handle_brightness(builder: &Builder, conn: DBusConnection) -> Result<(), 
             label.set_text(&p.to_string());
             scale.set_value(p as f64);
         }
-    ))();
+    ));
 
     scale.connect_value_changed(glib::clone!(
         #[strong]
