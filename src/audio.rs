7d162c9cb539ed032bcba8ea3ea6dfc87f23452f
ViniiR <cvrm2004@gmail.com>
2026-08-06 23:32:53 -0300

    feat: add mute volume toggle

--- src/audio.rs
+++ src/audio.rs
@@ -1,12 +1,58 @@
+use std::rc::Rc;
+
 use gtk::{
     gio::{DBusCallFlags, DBusConnection},
-    glib::{self, g_warning, object::ObjectExt, variant::ToVariant, VariantTy},
-    prelude::RangeExt,
-    Builder, Label, Scale,
+    glib::{self, g_warning, object::ObjectExt, variant::ToVariant, SignalHandlerId, VariantTy},
+    prelude::{RangeExt, WidgetExt},
+    Box, Builder, GestureClick, Image, Label, Scale,
 };
 
-use crate::types::HandlerError;
 use crate::types::{dbus, Program};
+use crate::{types::HandlerError, ui::get_volume_icon};
+
+async fn update_volume(
+    conn: DBusConnection,
+    label: Label,
+    image: Image,
+    scale: Scale,
+    signal: &SignalHandlerId,
+) {
+    let res = conn
+        .call_future(
+            Some(Program::BACKEND_NAME),
+            dbus::Controllers::AUDIO,
+            &dbus::Controllers::to_interface(dbus::Controllers::AUDIO),
+            dbus::Methods::GET_AUDIO,
+            None,
+            Some(VariantTy::TUPLE),
+            DBusCallFlags::NONE,
+            dbus::Timeout::NONE,
+        )
+        .await;
+
+    if let Err(e) = &res {
+        g_warning!(None, "DBus call error: {e:?}");
+        return;
+    }
+    let res = res.unwrap();
+
+    let percentage = res.child_value(0).get::<u32>();
+    let is_muted = res.child_value(1).get::<bool>();
+
+    match (percentage, is_muted) {
+        (Some(percentage), Some(is_muted)) => {
+            label.set_text(&format!("{}", percentage));
+            image.set_icon_name(Some(&get_volume_icon(percentage, is_muted)));
+
+            scale.block_signal(signal);
+            scale.set_value(percentage as f64);
+            scale.unblock_signal(signal);
+        }
+        _ => {
+            g_warning!(None, "GetAudio callback returned invalid tuple types");
+        }
+    }
+}
 
 pub fn handle_audio(builder: &Builder, conn: DBusConnection) -> Result<(), HandlerError<'_>> {
     let scale = builder
@@ -17,6 +63,18 @@ pub fn handle_audio(builder: &Builder, conn: DBusConnection) -> Result<(), Handl
         .ok_or(HandlerError::ObjectError(
             "Failed to get volume-scale-label",
         ))?;
+    let label_box =
+        builder
+            .object::<Box>("volume-scale-label-box")
+            .ok_or(HandlerError::ObjectError(
+                "Failed to get volume-scale-label-box",
+            ))?;
+    let label_image =
+        builder
+            .object::<Image>("volume-scale-label-image")
+            .ok_or(HandlerError::ObjectError(
+                "Failed to get volume-scale-label-image",
+            ))?;
 
     let signal = scale.connect_value_changed(glib::clone!(
         #[weak]
@@ -48,6 +106,7 @@ pub fn handle_audio(builder: &Builder, conn: DBusConnection) -> Result<(), Handl
             });
         }
     ));
+    let shared_id = Rc::new(signal);
 
     // Get volume on startup
     glib::spawn_future_local(glib::clone!(
@@ -55,44 +114,61 @@ pub fn handle_audio(builder: &Builder, conn: DBusConnection) -> Result<(), Handl
         label,
         #[weak]
         scale,
+        #[strong]
+        conn,
+        #[strong]
+        label_image,
+        #[strong]
+        shared_id,
         async move {
-            let res = conn
-                .call_future(
-                    Some(Program::BACKEND_NAME),
-                    dbus::Controllers::AUDIO,
-                    &dbus::Controllers::to_interface(dbus::Controllers::AUDIO),
-                    dbus::Methods::GET_AUDIO,
-                    None,
-                    Some(VariantTy::TUPLE),
-                    DBusCallFlags::NONE,
-                    dbus::Timeout::NONE,
-                )
-                .await;
-
-            if let Err(e) = &res {
-                g_warning!(None, "DBus call error: {e:?}");
-                return;
-            }
-            let res = res.unwrap();
-
-            let percentage = res.child_value(0).get::<u32>();
-            let is_muted = res.child_value(1).get::<bool>();
-
-            match (percentage, is_muted) {
-                (Some(percentage), Some(is_muted)) => {
-                    label.set_text(&format!("{}", percentage));
-
-                    // TODO: set is_muted on ui, and make it clickable
-                    scale.block_signal(&signal);
-                    scale.set_value(percentage as f64);
-                    scale.unblock_signal(&signal);
-                }
-                _ => {
-                    g_warning!(None, "GetAudio callback returned invalid tuple types");
-                }
-            }
+            update_volume(conn, label, label_image, scale, &shared_id).await;
         }
     ));
+
+    let click_controller = GestureClick::new();
+    click_controller.connect_pressed(glib::clone!(
+        #[weak]
+        label,
+        #[weak]
+        scale,
+        #[strong]
+        conn,
+        #[strong]
+        label_image,
+        #[strong]
+        shared_id,
+        move |_gesture, _n_press, _x, _y| {
+            glib::spawn_future_local(glib::clone!(
+                #[strong]
+                label_image,
+                #[strong]
+                conn,
+                #[strong]
+                shared_id,
+                async move {
+                    let res = conn
+                        .call_future(
+                            Some(Program::BACKEND_NAME),
+                            dbus::Controllers::AUDIO,
+                            &dbus::Controllers::to_interface(dbus::Controllers::AUDIO),
+                            dbus::Methods::TOGGLE_AUDIO_MUTED,
+                            None,
+                            None,
+                            DBusCallFlags::NONE,
+                            dbus::Timeout::NONE,
+                        )
+                        .await;
+                    if let Err(e) = &res {
+                        g_warning!(None, "DBus call error: {e:?}");
+                        return;
+                    }
+
+                    update_volume(conn, label, label_image, scale, &shared_id).await;
+                }
+            ));
+        }
+    ));
+    label_box.add_controller(click_controller);
 
     Ok(())
 }
--- src/ui.rs
+++ src/ui.rs
@@ -7,3 +7,15 @@ pub fn update_button_active(btn: &Button, to_active: bool) {
     btn.add_css_class(if to_active { ACTIVE } else { INACTIVE });
     btn.remove_css_class(if to_active { INACTIVE } else { ACTIVE });
 }
+
+pub fn get_volume_icon(percentage: u32, is_muted: bool) -> String {
+    if is_muted {
+        return String::from("audio-volume-muted-symbolic");
+    }
+
+    match percentage {
+        0..=40 => String::from("audio-status-volume-low-symbolic"),
+        41..=60 => String::from("audio-status-volume-medium-symbolic"),
+        _ => String::from("audio-status-volume-high-symbolic"),
+    }
+}
