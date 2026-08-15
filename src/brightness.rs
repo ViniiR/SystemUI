b62005781f17f22cf6d2973b7e78d39cc4d68f07
ViniiR <cvrm2004@gmail.com>
2026-08-15 12:05:54 -0300

    fix: add brightness scale timeout

--- src/brightness.rs
+++ src/brightness.rs
@@ -1,6 +1,8 @@
+use std::{cell::RefCell, rc::Rc};
+
 use gtk::{
     gio::{DBusCallFlags, DBusConnection},
-    glib::{self, g_warning, object::ObjectExt, variant::ToVariant, VariantTy},
+    glib::{self, g_warning, object::ObjectExt, variant::ToVariant, SignalHandlerId, VariantTy},
     prelude::RangeExt,
     Builder, Label, Scale,
 };
@@ -19,6 +21,9 @@ pub fn handle_brightness(builder: &Builder, conn: DBusConnection) -> Result<(), 
                 "Failed to get brightness-scale-label",
             ))?;
 
+    let pending_timer: Rc<RefCell<Option<glib::SourceId>>> = Rc::new(RefCell::new(None));
+    let last_sent_value = Rc::new(std::cell::Cell::new(u32::MAX));
+
     let signal = scale.connect_value_changed(glib::clone!(
         #[weak]
         label,
@@ -27,26 +32,50 @@ pub fn handle_brightness(builder: &Builder, conn: DBusConnection) -> Result<(), 
         move |scale| {
             let value = scale.value() as u32;
 
-            label.set_text(&format!("{}", value));
+            update_brightness(value, label, None, None);
 
-            let res = conn.call_future(
-                Some(Program::BACKEND_NAME),
-                dbus::Controllers::BRIGHTNESS,
-                &dbus::Controllers::to_interface(dbus::Controllers::BRIGHTNESS),
-                dbus::Methods::SET_BRIGHTNESS,
-                Some(&(value,).to_variant()),
-                None,
-                DBusCallFlags::NONE,
-                dbus::Timeout::NONE,
+            if value == last_sent_value.get() {
+                return;
+            }
+            if let Some(source_id) = pending_timer.borrow_mut().take() {
+                source_id.remove();
+            }
+
+            let timer_clone = pending_timer.clone();
+            let last_sent_clone = last_sent_value.clone();
+
+            let source_id = glib::timeout_add_local_once(
+                std::time::Duration::from_millis(50),
+                glib::clone!(
+                    #[strong]
+                    conn,
+                    move || {
+                        timer_clone.borrow_mut().take();
+                        last_sent_clone.set(value);
+
+                        let res = conn.call_future(
+                            Some(Program::BACKEND_NAME),
+                            dbus::Controllers::BRIGHTNESS,
+                            &dbus::Controllers::to_interface(dbus::Controllers::BRIGHTNESS),
+                            dbus::Methods::SET_BRIGHTNESS,
+                            Some(&(value,).to_variant()),
+                            None,
+                            DBusCallFlags::NONE,
+                            dbus::Timeout::NONE,
+                        );
+
+                        glib::spawn_future_local(async move {
+                            let res = res.await;
+
+                            if let Err(e) = &res {
+                                g_warning!(None, "DBus call error: {e:?}");
+                            }
+                        });
+                    }
+                ),
             );
 
-            glib::spawn_future_local(async move {
-                let res = res.await;
-
-                if let Err(e) = &res {
-                    g_warning!(None, "DBus call error: {e:?}");
-                }
-            });
+            *pending_timer.borrow_mut() = Some(source_id);
         }
     ));
 
@@ -57,35 +86,47 @@ pub fn handle_brightness(builder: &Builder, conn: DBusConnection) -> Result<(), 
         #[weak]
         scale,
         async move {
-            let res = conn
-                .call_future(
-                    Some(Program::BACKEND_NAME),
-                    dbus::Controllers::BRIGHTNESS,
-                    &dbus::Controllers::to_interface(dbus::Controllers::BRIGHTNESS),
-                    dbus::Methods::GET_BRIGHTNESS,
-                    None,
-                    Some(VariantTy::TUPLE),
-                    DBusCallFlags::NONE,
-                    dbus::Timeout::NONE,
-                )
-                .await;
+            let call = conn.call_future(
+                Some(Program::BACKEND_NAME),
+                dbus::Controllers::BRIGHTNESS,
+                &dbus::Controllers::to_interface(dbus::Controllers::BRIGHTNESS),
+                dbus::Methods::GET_BRIGHTNESS,
+                None,
+                Some(VariantTy::TUPLE),
+                DBusCallFlags::NONE,
+                dbus::Timeout::NONE,
+            );
 
-            if let Err(e) = &res {
-                g_warning!(None, "DBus call error: {e:?}");
-                return;
+            match call.await {
+                Ok(r) => {
+                    let Some(p) = r.child_value(0).get::<u32>() else {
+                        g_warning!(None, "GetBrightness callback returned incorrect value");
+                        return;
+                    };
+
+                    update_brightness(p, label, Some(scale), Some(&signal));
+                }
+                Err(e) => {
+                    g_warning!(None, "DBus call error: {e:?}");
+                }
             }
-            let Some(p) = res.unwrap().child_value(0).get::<u32>() else {
-                g_warning!(None, "GetBrightness callback returned incorrect value");
-                return;
-            };
-
-            label.set_text(&format!("{}", p));
-
-            scale.block_signal(&signal);
-            scale.set_value(p as f64);
-            scale.unblock_signal(&signal);
         }
     ));
 
     Ok(())
+}
+
+fn update_brightness(
+    percentage: u32,
+    label: Label,
+    scale: Option<Scale>,
+    signal: Option<&SignalHandlerId>,
+) {
+    label.set_text(&format!("{}", percentage));
+
+    if let (Some(s), Some(scl)) = (signal, scale) {
+        scl.block_signal(s);
+        scl.set_value(percentage as f64);
+        scl.unblock_signal(s);
+    }
 }
